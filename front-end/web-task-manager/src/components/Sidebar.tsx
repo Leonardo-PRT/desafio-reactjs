fa459a560ebacb1c095bd52dfd43726bfab80b42
LeonardoPorto <leonardo.porto@evolutix.com.br>
2024-06-17 14:19:11 -0300

    fixing drag n drop

--- front-end/web-task-manager/src/components/Sidebar.tsx
+++ front-end/web-task-manager/src/components/Sidebar.tsx
@@ -1,15 +1,13 @@
 import React, { useState, useContext } from 'react';
-import {Link} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import CreateProjectModal from './CreateProjectModal';
 import '../styles/main.css';
-import {ProjectContext} from "../context/ProjectContext.tsx";
+import { ProjectContext } from "../context/ProjectContext.tsx";
 
 const Sidebar: React.FC = () => {
-
     const { user, projects, loadProjects } = useContext(ProjectContext);
     const [isModalOpen, setIsModalOpen] = useState(false);
-
-
+    const navigate = useNavigate();
 
     const handleOpenModal = () => {
         setIsModalOpen(true);
@@ -23,18 +21,23 @@ const Sidebar: React.FC = () => {
         loadProjects();
     };
 
+    const handleProjectClick = (projectId: number) => {
+        navigate(`/projects/${projectId}`);
+        window.location.reload();
+    };
+
     return (
         <div className="sidebar">
             {user && <div className="user-info">{user.name}</div>}
             <ul className="project-list">
                 {projects.map(project => (
                     <li key={project.id} className="project-item">
-                        <Link to={`/projects/${project.id}`} className="project-link">
+                        <div className="project-link" onClick={() => handleProjectClick(project.id)}>
                             <div className="project-container">
                                 <span className="project-name">{project.name}</span>
                                 <span className="task-count">{project.tasksCount}</span>
                             </div>
-                        </Link>
+                        </div>
                     </li>
                 ))}
             </ul>
